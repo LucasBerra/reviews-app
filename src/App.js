871591d3baf35d7140290dfa35f2c52d1ea19591
LucasBerra <berra.lucas.santiago@gmail.com>
2021-10-16 15:33:52 -0300

    Upload main Review App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+import reviews from "./data";
+
+const App = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = reviews[index];
+
+  const handlePrev = () => {
+    if (index === 0) {
+      setIndex(reviews.length - 1);
+    } else {
+      setIndex(index - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (index === reviews.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>
+        <i className="fas fa-highlighter"></i> Our Reviews :
+      </h1>
+      <div className="review">
+        <div className="photo">
+          <img src={image} alt={name} />
+          <i className="fas fa-quote-right"></i>
+        </div>
+        <div>
+          <h3>{name}</h3>
+          <p>
+            <code>{job}</code>
+          </p>
+
+          <p className="text">{text}</p>
+
+          <button onClick={handlePrev}>
+            <i className="fas fa-chevron-left"></i>
+          </button>
+          <button onClick={handleNext}>
+            <i className="fas fa-chevron-right"></i>
+          </button>
+        </div>
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
