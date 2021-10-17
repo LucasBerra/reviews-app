45b7d697e043236be7cd5c440c623e2b84e6669d
LucasBerra <berra.lucas.santiago@gmail.com>
2021-10-17 12:40:15 -0300

    Replace placeholder lorem ipsum in data.js

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ const reviews = [
     job: "web developer",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    text: "Here I've been able to work with wonderful people and the best tools available to date. I can confidently say I've improved as a developer thanks to the great people I've met!",
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ const reviews = [
     job: "web designer",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    text: "Communication was key in having this be a relaxing workplace. Many times I've struggled to get my point accross to the rest of a dev team; not this time. I've enjoyed my time here a lot.",
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ const reviews = [
     job: "intern",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    text: "I've learned a lot from my time here, putting my skills to the test every day and every chance I could. I'm very happy to say I've grown from this experience and I'd be glad to work here again!",
   },
   {
     id: 4,
@@ -29,7 +29,7 @@ const reviews = [
     job: "the boss",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.",
+    text: "As my first experience being the lead of a team, I'm quite pleased with the quality of both the work environment and coworkers. This shall be my new minimum standard in future projects.",
   },
 ];
 
